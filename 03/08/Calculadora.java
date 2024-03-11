34b9c4933108e3c79056e48daacfa00b170e4b06
Nicollas <132391248+NicollasDavi@users.noreply.github.com>
2024-03-11 09:09:38 -0300

    .

---
+++ 03/08/Calculadora.java
@@ -0,0 +1,24 @@
+public class Calculadora {
+
+    public static void imprimir(String texto){
+        System.out.println(texto);
+    }
+
+    public static int somar(int a, int b){
+        int r = a + b;
+        return r;
+    }
+    public static int subtrair(int a, int b){
+        int r = a - b;
+        return r;
+    }
+    public static int multiplicar(int a, int b){
+        int r = a * b;
+        return r;
+    }
+    public static int dividir(int a, int b){
+        int r = a / b;
+        return r;
+    }
+    
+}
